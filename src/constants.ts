afc05b29ddf761aea0f7c477b0479ba397529a2c
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-07-20 12:16:22 +0200

    docs: update comments

--- src/constants.ts
+++ src/constants.ts
@@ -18,8 +18,8 @@ export const ROMANS = [
 // O'Reilly Strict
 // modified to not match empty strings ([^ ]) and added a negative lookbehind to not partially match invalid numerals
 // https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch06s09.html
-// This doesn't work since it matches some empty strings
 export const ROMANS_RXP =
   /(?<![MDCLXVI])(?=[MDCLXVI])M{0,3}(?:C[MD]|D?C{0,3})(?:X[CL]|L?X{0,3})(?:I[XV]|V?I{0,3})[^ ]\b/g;
 
+// Matches decimals 1-3999
 export const DECIMALS_RXP = /\b([1-3]?[0-9]?[0-9]?[0-9])\b/g;
